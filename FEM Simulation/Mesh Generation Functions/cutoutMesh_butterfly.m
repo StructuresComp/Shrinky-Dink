646616d5dfb7f4a305476cbd5e09cf365427dc37
mrunzzz <mrunmayiggt@gmail.com>
2024-08-30 18:41:53 -0700

    Change butterfly mouse meshes

--- FEM Simulation/Mesh Generation Functions/cutoutMesh_butterfly.m
+++ FEM Simulation/Mesh Generation Functions/cutoutMesh_butterfly.m
@@ -1,18 +1,120 @@
 function [FEMesh, cutOutElements] = cutoutMesh_butterfly(stlfile)
+% model = createpde;
+% 
+% gm = importGeometry(model,stlfile);
+% 
+% scale(gm,0.001);
+% center = mean(gm.Vertices);
+% translate(gm,[-center(1),-center(2)])
+% figure(1)
+% pdegplot(model,"FaceLabels","on")
+% figure(2)
+% FEMesh = generateMesh(model,"Hedge",{1:gm.NumEdges,0.0001});
+% pdemesh(FEMesh)
+% hold on
+% cutOutElements = findElements(FEMesh,'region','Face',[3 6 7 11]); % Define the kirigiami elements
+% pdemesh(FEMesh.Nodes, FEMesh.Elements(:,cutOutElements),'EdgeColor','green');
+% hold off
+
+x_shrinky = [0 -0.024 -0.032 -0.033 -0.032 -0.03 -0.012 -0.008 -0.002];
+x_shrinky = [x_shrinky flip(-x_shrinky(2:end))];
+
+% y_shrinky = [0.02 0.045 0.046 -0.005 -0.010 -0.015 -0.034 -0.035 -0.036];
+y_shrinky = [0.02 0.045 0.046 -0.005 -0.010 -0.015 -0.033 -0.035 -0.036];
+y_shrinky = [y_shrinky flip(y_shrinky(2:end))];
+
+
+x_top_out_left = [-0.021 -0.029 -0.03 -0.024 -0.0004 -0.001 -0.005 -0.022]-0.002;
+y_top_out_left = [0.04 0.045 0.005 -0.002 -8e-6 0.007 0.01 0.031];
+
+x_top_out_right = -x_top_out_left;
+y_top_out_right = y_top_out_left;
+
+x_top_in_1_left = [-0.025 -0.022 -0.026 -0.029]-0.002;
+y_top_in_1_left = [0.024 0.019 0.0071 0.0076];
+
+x_top_in_1_right= -x_top_in_1_left;
+y_top_in_1_right = y_top_in_1_left;
+
+
+x_top_in_2_left = [-0.025 -0.017 -0.014 -0.025]-0.002;
+y_top_in_2_left = [0.005 0.013 0.007 0.001];
+
+x_top_in_2_right = -x_top_in_2_left;
+y_top_in_2_right = y_top_in_2_left;
+
+% x_bot_out_left = [-0.031 -0.005 -0.001 -0.002 -0.011 -0.009 -0.023 -0.029];
+% y_bot_out_left = [-0.0030   -0.0020   -0.0060   -0.0320   -0.0300   -0.0230   -0.0100   -0.0110];
+% 
+% x_bot_out_right = -x_bot_out_left;
+% y_bot_out_right = y_bot_out_left;
+% 
+% x_bot_in_1_left = [-0.007 -0.0067 -0.015 -0.017];
+% y_bot_in_1_left = [ -0.0040   -0.0070   -0.0120   -0.0070];
+% 
+% x_bot_in_1_right = -x_bot_in_1_left;
+% y_bot_in_1_right = y_bot_in_1_left;
+% 
+% x_bot_in_2_left = [-0.005 -0.003 -0.006 -0.01];
+% y_bot_in_2_left = [-0.0070   -0.0070   -0.0190   -0.0160];
+% 
+% x_bot_in_2_right = -x_bot_in_2_left;
+% y_bot_in_2_right = y_bot_in_2_left;
+
+x_bot_out_left = [-0.031 -0.005 -0.001 -0.002 -0.011 -0.009 -0.023 -0.029] - 0.002;
+y_bot_out_left = [-0.0030   -0.0020   -0.0060   -0.0320   -0.0300   -0.0230   -0.0100   -0.0110];
+
+x_bot_out_right = -x_bot_out_left;
+y_bot_out_right = y_bot_out_left;
+
+x_bot_in_1_left = [-0.007 -0.0067 -0.015 -0.017]-0.002;
+y_bot_in_1_left = [ -0.0040   -0.0070   -0.0120   -0.0070];
+
+x_bot_in_1_right = -x_bot_in_1_left;
+y_bot_in_1_right = y_bot_in_1_left;
+
+x_bot_in_2_left = [-0.005 -0.003 -0.006 -0.01]-0.002;
+y_bot_in_2_left = [-0.0070   -0.0070   -0.0190   -0.0160];
+
+x_bot_in_2_right = -x_bot_in_2_left;
+y_bot_in_2_right = y_bot_in_2_left;
+
+
+
+gd = zeros(1000,13);
+gd(1:2+2*length(x_shrinky),1) = [2 length(x_shrinky) x_shrinky y_shrinky]';
+gd(1:2+2*length(x_top_out_left),2) = [2 length(x_top_out_left) x_top_out_left y_top_out_left]';
+gd(1:2+2*length(x_top_out_right),3) = [2 length(x_top_out_right) x_top_out_right y_top_out_right]';
+gd(1:2+2*length(x_top_in_1_left),4) = [2 length(x_top_in_1_left) x_top_in_1_left y_top_in_1_left]';
+gd(1:2+2*length(x_top_in_1_right),5) = [2 length(x_top_in_1_right) x_top_in_1_right y_top_in_1_right]';
+gd(1:2+2*length(x_top_in_2_left),6) = [2 length(x_top_in_2_left) x_top_in_2_left y_top_in_2_left]';
+gd(1:2+2*length(x_top_in_2_right),7) = [2 length(x_top_in_2_right) x_top_in_2_right y_top_in_2_right]';
+gd(1:2+2*length(x_bot_out_left),8) = [2 length(x_bot_out_left) x_bot_out_left y_bot_out_left]';
+gd(1:2+2*length(x_bot_out_right),9) = [2 length(x_bot_out_right) x_bot_out_right y_bot_out_right]';
+gd(1:2+2*length(x_bot_in_1_left),10) = [2 length(x_bot_in_1_left) x_bot_in_1_left y_bot_in_1_left]';
+gd(1:2+2*length(x_bot_in_1_right),11) = [2 length(x_bot_in_1_right) x_bot_in_1_right y_bot_in_1_right]';
+gd(1:2+2*length(x_bot_in_2_left),12) = [2 length(x_bot_in_2_left) x_bot_in_2_left y_bot_in_2_left]';
+gd(1:2+2*length(x_bot_in_2_right),13) = [2 length(x_bot_in_2_right) x_bot_in_2_right y_bot_in_2_right]';
+
+
+g = decsg(gd);
 model = createpde;
+geometryFromEdges(model,g);
 
-gm = importGeometry(model,stlfile);
 
-figure(1)
-pdegplot(model,"FaceLabels","on")
-scale(gm,0.001);
-center = mean(gm.Vertices);
-translate(gm,[-center(1),-center(2)])
+%% Plot the geometry (for simple check)
+
+figure(1);
+pdegplot(model,'FaceLabels','on')
+axis equal
+
 figure(2)
-FEMesh = generateMesh(model,"Hedge",{1:gm.NumEdges,0.0001});
-pdemesh(FEMesh,"NodeLabels","on")
+FEMesh = generateMesh(model);
+pdemesh(FEMesh)
 hold on
-cutOutElements = findElements(FEMesh,'region','Face',[3 6 7 11]); % Define the kirigiami elements
+cutOutElements = findElements(FEMesh,'region','Face',[2 5 7 9]); % Define the kirigiami elements
 pdemesh(FEMesh.Nodes, FEMesh.Elements(:,cutOutElements),'EdgeColor','green');
 hold off
+
 end
+
--- FEM Simulation/Mesh Generation Functions/cutoutMesh_mouse.m
+++ FEM Simulation/Mesh Generation Functions/cutoutMesh_mouse.m
@@ -1,15 +1,71 @@
 function [FEMesh, cutOutElements] = cutoutMesh_mouse(stlfile)
+% model = createpde;
+% 
+% gm = importGeometry(model,stlfile);
+% 
+% figure(1)
+% pdegplot(model,"FaceLabels","on")
+% scale(gm,0.001);
+% center = mean(gm.Vertices);
+% translate(gm,[-center(1),-center(2)])
+% 
+% pdegplot(model,"FaceLabels","on")
+% figure(2)
+% FEMesh = generateMesh(model,"Hedge",{1:gm.NumEdges,0.0001});
+% pdemesh(FEMesh)
+% hold on
+% cutOutElements = findElements(FEMesh,'region','Face',1); % Define the kirigiami elements
+% pdemesh(FEMesh.Nodes, FEMesh.Elements(:,cutOutElements),'EdgeColor','green');
+% hold off
+
+x_shrinky = [-0.026 -0.035 -0.036 -0.03 -0.02 -0.016 -0.008 0];
+x_shrinky = [x_shrinky flip(-x_shrinky(1:end-1))];
+
+y_shrinky = [-0.054 -0.034 0.012 0.037 0.038 0.038 0.037 0.02];
+y_shrinky = [y_shrinky flip(y_shrinky(1:end-1))];
+
+
+x_cut_left = [-0.035 -0.029 -0.026 -0.024 -0.027 -0.032 -0.036];
+y_cut_left = [-0.034 -0.023 -0.016 -0.009 0 0.006 0.012];
+
+x_cut_right = -x_cut_left;
+y_cut_right = y_cut_left;
+
+x_cut_top = [0 -0.008 -0.009 -0.009 -0.008 -0.006];
+x_cut_top = [x_cut_top flip(-x_cut_top(2:end))];
+
+y_cut_top = [0.02 0.037 0.027 0.024 0.018 0.013];
+y_cut_top = [y_cut_top flip(y_cut_top(2:end))];
+
+x_cut_bottom = [0 -0.007 -0.016 -0.026];
+x_cut_bottom = [x_cut_bottom flip(-x_cut_bottom(2:end))];
+
+y_cut_bottom = [-0.044 -0.045 -0.049 -0.054];
+y_cut_bottom = [y_cut_bottom flip(y_cut_bottom(2:end))];
+
+
+
+
+gd = zeros(2+2*length(x_shrinky),5);
+gd(:,1) = [2 length(x_shrinky) x_shrinky y_shrinky]';
+gd(1:2+2*length(x_cut_left),2) = [2,length(x_cut_left),x_cut_left,y_cut_left]';
+gd(1:2+2*length(x_cut_right),3) = [2,length(x_cut_right),x_cut_right,y_cut_right]';
+gd(1:2+2*length(x_cut_top),4) = [2,length(x_cut_top),x_cut_top,y_cut_top]';
+gd(1:2+2*length(x_cut_bottom),5) = [2,length(x_cut_bottom),x_cut_bottom,y_cut_bottom]';
+
+g = decsg(gd);
 model = createpde;
+geometryFromEdges(model,g);
 
-gm = importGeometry(model,stlfile);
 
-figure(1)
-pdegplot(model,"FaceLabels","on")
-scale(gm,0.001);
-center = mean(gm.Vertices);
-translate(gm,[-center(1),-center(2)])
+%% Plot the geometry (for simple check)
+
+figure(1);
+pdegplot(model,'FaceLabels','on')
+axis equal
+
 figure(2)
-FEMesh = generateMesh(model,"Hedge",{1:36,0.0001});
+FEMesh = generateMesh(model, "Hmax",0.001);
 pdemesh(FEMesh)
 hold on
 cutOutElements = findElements(FEMesh,'region','Face',1); % Define the kirigiami elements
